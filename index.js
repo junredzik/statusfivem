853c47c3fcf83e892e7478ebbfd2a66f9ba31511
junredzik <87615205+junredzik@users.noreply.github.com>
2023-08-19 01:26:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,43 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const request = require('request');
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent]
+});
+
+let lastStatusIndicator = null;
+
+client.on('ready', () => {
+  console.log(`[junredzik] Zalogowano jako: ${client.user.tag} (${client.user.id})`);
+  updateStatus();
+  setInterval(updateStatus, 1000 * 30);
+});
+
+const updateStatus = async (channel = null) => {
+  try {
+    const body = await request('https://lambda.fivem.net');
+    const online = JSON.stringify(body).includes("CleanAndLegit");
+
+    console.log(`Sprawdzam status FiveM: ${online ? 'online' : 'offline'}`);
+
+    const activityName = online ? '``🟢 FiveM``' : '``🔴 FiveM``';
+
+    if (lastStatusIndicator !== online) {
+      lastStatusIndicator = online;
+      if (channel) {
+        channel.send(`Status FiveM: ${online ? 'Server Online' : 'Server Offline'}`);
+      }
+
+      const targetChannel = await client.channels.fetch('');
+      targetChannel.send(activityName);
+    }
+  } catch (error) {
+    console.log('Error: ', error);
+    if (channel) {
+      channel.send('Nie udało się pobrać statusu.');
+    }
+  }
+};
+
+const tokenik = '';
+client.login(tokenik);
